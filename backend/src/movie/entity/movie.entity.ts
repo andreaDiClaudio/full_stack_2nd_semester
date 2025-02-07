1cae2d543aec9554f3a75f57adb16ed712c6f9c8
andreaDiClaudio <andreadicla@gmail.com>
2025-02-07 10:33:01 +0100

    added comments

--- backend/src/movie/entity/movie.entity.ts
+++ backend/src/movie/entity/movie.entity.ts
@@ -1,1 +1,3 @@
+// Step 2: create with nest -g <module> folderName (same command for service, controller)
+// Step 3: create entity with nest -g class folderName/entityName --no-spec
 export class Movie {}
