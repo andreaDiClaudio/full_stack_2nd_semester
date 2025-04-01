dafffce681461cb4d973f9ee7210385ab7286c3f
andreaDiClaudio <andreadicla@gmail.com>
2025-04-01 14:59:19 +0200

    started in role based auth

---
+++ backend/src/authentication/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Strategy, ExtractJwt } from 'passport-jwt'
+import { Injectable } from '@nestjs/common';;
+import { jwtConstants } from './constants';
+import * as dotenv from 'dotenv';
+import { PassportStrategy } from '@nestjs/passport';
+dotenv.config();
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret, // This is in-secure because I am sharing my key in my code base
+      // secretOrKey: process.env.JWT_SECRET, // This is the recommended way but requires everyone in the development team to have access to the key (create their .env file)
+    });
+  }
+
+  async validate(payload: any) {
+    return {
+      id: payload.id,
+      username: payload.username,
+      //tenantId: payload.tenantId
+    };
+  }
+}
